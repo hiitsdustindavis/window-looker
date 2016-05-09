1cef383f49aa7a158b3b6d79946f74190d070566
dd <Guest@epicodus-20.local>
2016-05-09 16:33:51 -0700

    add bottom bar and scroll functionality

--- js/function.js
+++ js/function.js
@@ -9,24 +9,36 @@ $(document).ready(function(){
     var wScroll = $(this).scrollTop();
 
     $('.blind').each(function(i){
-      if($(window).height() < 1200){
-        $(this).css({
-          'transform' : 'rotateX(' + wScroll / 1.2 + 'deg)',
-          // 'transform' : 'translateY(' + -5 + 'vh)'
-        });
-      }
-      if($(window).height() < 780){
-        $(this).css({
-          'transform' : 'rotateX(' + wScroll / 1.2 + 'deg)',
-          // 'transform' : 'translateY(' + -5 + 'vh)'
-        });
-      } else {
-        $(this).css({
-          'transform' : 'rotateX(' + (wScroll / 2) + 'deg)'
-          // 'transform' : 'translateY(' + 5 + 'vh)'
-        });
-      } console.log(wScroll / 1.2);
-      // console.log(wScroll);
-    });
+      if(wScroll < 187) {
+        if($(window).height() < 1200){
+          $(this).css({
+            'transform' : 'rotateX(' + wScroll / 1.2 + 'deg)',
+            // 'transform' : 'translateY(' + -5 + 'vh)'
+          });
+        }
+        if($(window).height() < 780){
+          $(this).css({
+            'transform' : 'rotateX(' + wScroll / 1.2 + 'deg)',
+            // 'transform' : 'translateY(' + -5 + 'vh)'
+          });
+        } else {
+          $(this).css({
+            'transform' : 'rotateX(' + (wScroll / 2) + 'deg)'
+            // 'transform' : 'translateY(' + 5 + 'vh)'
+          });
+        } console.log(wScroll / 1.2);
+        // console.log(wScroll);
+  } else if($('.bottom-bar').css("position") === "fixed") {
+    console.log("asdsdd")
+      $('.bottom-bar').css({
+        "position" : "relative",
+        "top" : (wScroll + $(window).height()) + $('.bottom-bar').height()
+      })
+      // $('.bottom-bar').css({
+      //   "bottom" : parseInt($('.bottom-bar').css("bottom")) + 1
+      // });
+    }
+
   });
+});
 });
