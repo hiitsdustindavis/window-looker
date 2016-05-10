6efc729edc4833b956a3aee912bfc46e260c4270
dd <Guest@epicodus-20.local>
2016-05-09 17:44:57 -0700

    hotfix

--- js/function.js
+++ js/function.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
   });
   });
   $(window).scroll(function(){
+    //Generally, what is happening here is on every scroll this whole function is firing. First it checks to see if wScroll < 187. If it is, it will alter the transform:rotate property of the .blind <div>. Otherwise if wScroll is > 187 it will stop altering the transform:rotate property move on to the next elseif branch (explained below).
     var wScroll = $(this).scrollTop();
 
     $('.blind').each(function(i){
@@ -28,8 +29,10 @@ $(document).ready(function(){
           });
         } console.log(wScroll / 1.2);
         // console.log(wScroll);
+
+    //Else if below = div with class "bottom-bar" has position "fixed" execute contents.
   } else if($('.bottom-bar').css("position") === "fixed") {
-    console.log("asdsdd")
+    //Change "bottom-bar" div position to "relative" and it's top so that it stays in the same position. We are defining its position from the top of the body. To figure out that distance we add the amount of pixels scrolled (wScroll) to the whole height of the window ($(window).height())) and subtract the height of the bottom bar
       $('.bottom-bar').css({
         "position" : "relative",
         "top" : (wScroll + $(window).height()) + $('.bottom-bar').height()
